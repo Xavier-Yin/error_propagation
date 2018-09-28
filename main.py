d329e0994151a3b30bcc8ef99b4491fc745afdee
XavierYin <xavier@candyhouse.co>
2018-09-28 10:24:50 +0800

    modify syntax to fit both Python 2 & 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def print_stats(var, var_name="unnamed_variable"):
-    print "{v} has mean = {m} and stdev = {s} ({n} samples)".format(v=var_name, m=var.mean(), s=var.std(), n=len(var))
+    print("{v} has mean = {m} and stdev = {s} ({n} samples)".format(v=var_name, m=var.mean(), s=var.std(), n=len(var)))
 
 def plot_hist(var, var_name="unnamed_variable", bin_count=30):
     count, bins, ignored = plt.hist(var, bin_count, label=var_name)
@@ -30,7 +30,7 @@ def demo():
     """
     N_SAMPLE = 1000000
     mu, sigma = 0, 1
-    print "N_SAMPLE={n}, mu={m}, sigma={s}".format(n=N_SAMPLE, m=mu, s=sigma)
+    print("N_SAMPLE={n}, mu={m}, sigma={s}".format(n=N_SAMPLE, m=mu, s=sigma))
     x = np.random.normal(mu, sigma, N_SAMPLE)   # N_SAMPLE-sized array of random variable x
     y = x**2 + 2*x + 1                          # N_SAMPLE-sized array of deduced variable y
 #     present_var(x, "x")
